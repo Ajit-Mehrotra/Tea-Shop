c15bf25c58026741a8f082cf8157c4c8a6fbf54c
Ajit Mehrotra <ajitmehrotra@Ajits-MacBook-Air.local>
2024-08-24 23:07:29 -0400

    work in progress

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+
+        <nav className="border p-4">
+            <h1 className="text-normal text-center">Hello</h1>
+        </nav>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,21 @@
 
 "use client"
 import ThemeToggle from "./components/ThemeToggle";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
 
 
 
   return (
-    <main className="bg-bg flex items-center justify-center p-20 pt-32">
+    <main className="bg-bg p-20 pt-10">
+      <Navbar />
       <div>
-        <h1 className="max-w-3xl text-center font-bold text-slate-900 dark:text-cyan-500 text-5xl leading-tight mb-3">Tailwind CSS: Dark Mode Tutorial</h1>
-        <p className="text-lg font-medium text-slate-700 dark:text-cyan-700 text-center mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam corporis officia illum saepe voluptates, assumenda molestiae exercitationem quisquam illo omnis? Fuga, voluptates? Eum dolor ipsam expedita perspiciatis doloremque, ad illo!</p>
+        <h1 className="max-w-3xl text-center font-bold text-primary text-5xl leading-tight mb-3">Spill Tea with your people, privately.</h1>
+        <p className="text-lg font-medium text-normal text-center mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam corporis officia illum saepe voluptates, assumenda molestiae exercitationem quisquam illo omnis? Fuga, voluptates? Eum dolor ipsam expedita perspiciatis doloremque, ad illo!</p>
         <button className="bg-normal text-primary font-semibold py-2 px-4 rounded-md">Get Started</button>
         <ThemeToggle />
       </div>
-    </main>
+    </main >
   );
 }
