c7139f8b6c152c0e83771d5ca9ee2fc5c0ce200c
Ajit Mehrotra <ajitmehrotra@Ajits-MacBook-Air.local>
2024-08-31 23:34:12 -0400

    work in progress

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -3,6 +3,18 @@ export default function Navbar() {
 
         <nav className="border p-4">
             <h1 className="text-normal text-center">Hello</h1>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/blog">Blog</a>
+                </li>
+                <li>
+                    <a href="/profile">Profile</a>
+                </li>
+
+            </ul>
         </nav>
     )
 }
